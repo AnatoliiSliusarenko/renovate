b1f7a887082b1c71647f865def0cf0d4bc9dbcb1
Anatoliy Duhovniy <dj.slyusar@gmail.com>
2015-01-03 01:46:20 +0200

    contacts

--- src/Ordis/MainBundle/Controller/ContactsController.php
+++ src/Ordis/MainBundle/Controller/ContactsController.php
@@ -3,11 +3,28 @@
 namespace Ordis\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactsController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('OrdisMainBundle:Contacts:index.html.twig');
+    	$parameters = array();
+    	if ($request->getMethod() == 'POST')
+    	{
+    		$message = \Swift_Message::newInstance()
+    					->setSubject($request->request->get('topic'))
+    					->setFrom('dj.slyusar@gmail.com')
+    					->setTo('dj.slyusar@rambler.ru')
+    					->setBody(
+    					"П.І.П.: ".$request->request->get('pip')."\n".
+    					"Email: ".$request->request->get('email')."\n\n".		
+    					$request->request->get('message'));
+    		
+    		$this->get('mailer')->send($message);
+    		$parameters['message_result'] = 'Ваш лист успішно надіслано!';
+    	}
+    	
+        return $this->render('OrdisMainBundle:Contacts:index.html.twig',$parameters);
     }
 }
