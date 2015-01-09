db47ac93c09e90f68f818e1edba157df6297fa6f
Anatoliy Duhovniy <dj.slyusar@gmail.com>
2015-01-10 01:20:26 +0200

    remove derective

--- web/bundles/renovate/js/angularInitialization.js
+++ web/bundles/renovate/js/angularInitialization.js
@@ -3,28 +3,6 @@ var Renovate = angular.module('Renovate',['ui.bootstrap'])
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
     $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-}])
-.directive("repeatPassword", function(){
-	return {
-        require: "ngModel",
-        link: function(scope, elem, attrs, ctrl) {
-        	if (elem === undefined) return;
-            var otherInput = elem.inheritedData("$formController")[attrs.repeatPassword];
-
-            ctrl.$parsers.push(function(value) {
-                if(value === otherInput.$viewValue) {
-                    ctrl.$setValidity("repeat", true);
-                    return value;
-                }
-                ctrl.$setValidity("repeat", false);
-            });
-
-            otherInput.$parsers.push(function(value) {
-                ctrl.$setValidity("repeat", value === ctrl.$viewValue);
-                return value;
-            });
-        }
-    };
-});
+}]);
 
 console.log('Angular Initialization finished...');
